2191ba6f5cae97a07756dee4cadd9e44b41d37bc
Matias Arriola <matias@eyeseetea.com>
2025-05-12 11:13:28 -0300

    chore: add icon + author

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,10 @@
 const config = {
   name: "zipcode-lookup-plugin",
-  title: "Zipcode Lookup Plugin",
+  title: "Zipcode Lookup Capture Plugin",
   description:
     "A Capture Plugin to look up zipcodes and prefill city and state information",
   type: "app",
+  author: "EyeSeeTea team",
 
   entryPoints: {
     plugin: "./src/Plugin.tsx",
